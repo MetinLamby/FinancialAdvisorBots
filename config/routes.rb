75a1e4bc599f6df0bfa68a8ea193bdc3cdde1cc7
Metin Lamby <zsh git_setup.sh>
2020-11-24 13:18:47 +0100

    added the next two bots

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'increaseCapital', to: 'pages#increaseCapital',  as: :increaseCapital
+  get 'differenceAssetLiability', to: 'pages#differenceAssetLiability',  as: :differenceAssetLiability
+  get 'diversification', to: 'pages#diversification',  as: :diversification
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
